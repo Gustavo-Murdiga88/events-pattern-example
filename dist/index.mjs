90a3aa4897fbe35ad86f1f39964b9c6edd70e7dc
Gustavo <gumurdiga@gmail.com>
2023-12-16 18:17:26 -0300

    git: remove dist folder

--- dist/index.mjs
+++ dist/index.mjs
@@ -6,6 +6,119 @@ var Entity = class {
   }
 };
 
+// src/events/email.ts
+var Email = class extends Entity {
+  constructor({ from, to, content, date, title, user }) {
+    super({ from, to, content, date, title, user });
+  }
+};
+var SendEmail = class {
+  static sendEmail(entity) {
+    console.log("email", {
+      title: entity.props.title,
+      user: entity.props.user,
+      from: entity.props.from,
+      to: entity.props.to,
+      content: entity.props.content,
+      date: entity.props.date
+    });
+  }
+};
+
+// src/events/event-root.ts
+var EventRoot = class {
+  events = [];
+  subscribe({ cb, name, status }) {
+    const contains = this.events.find((event) => event.name === name);
+    if (contains) {
+      console.log(`event ${name} already exists`);
+      return;
+    }
+    this.events.push({
+      cb,
+      name,
+      status
+    });
+  }
+  dispatch(name) {
+    const events = this.events.filter(
+      (ev) => ev.name === name && ev.status === "canDispatch"
+    );
+    if (!events.length) {
+      console.log(`event ${name} not found`);
+    }
+    events.forEach((event) => {
+      event.cb();
+    });
+  }
+  unsubscribe(name) {
+    const indexEvent = this.events.findIndex((event) => event.name === name);
+    if (indexEvent === -1) {
+      console.log(`this even ${name} not found`);
+      return;
+    }
+    this.events.splice(indexEvent, 1);
+  }
+  changeStatus(name, status) {
+    const indexEvent = this.events.findIndex((event) => event.name === name);
+    if (indexEvent === -1) {
+      console.log(`this event ${name} not found`);
+      return;
+    }
+    this.events[indexEvent].status = status;
+  }
+};
+
+// src/events/messages.ts
+var Phone = class extends Entity {
+  constructor({ number, userName }) {
+    super({ number, userName });
+  }
+};
+var SendMessage = class {
+  static sendMessage(entity) {
+    console.log("phone", {
+      number: entity?.props.number,
+      userName: entity?.props.userName
+    });
+  }
+};
+
+// src/events/index.ts
+var eventRoot = new EventRoot();
+var myPhone = new Phone({
+  number: "123456789",
+  userName: "john"
+});
+var myEmail = new Email({
+  content: "Vero cum adipisci beatae voluptate voluptas.",
+  date: /* @__PURE__ */ new Date(),
+  from: "johndoe@johndoe.com",
+  title: "Hi dear this is johndoe",
+  to: "johndoe@johndoe.com",
+  user: "Gustavinho"
+});
+eventRoot.subscribe({
+  name: SendMessage.name,
+  cb: () => {
+    SendMessage.sendMessage(myPhone);
+  },
+  status: "added"
+});
+eventRoot.subscribe({
+  cb: () => {
+    SendEmail.sendEmail(myEmail);
+  },
+  name: SendEmail.name,
+  status: "added"
+});
+eventRoot.changeStatus(SendMessage.name, "canDispatch");
+eventRoot.changeStatus(SendEmail.name, "canDispatch");
+eventRoot.dispatch(SendMessage.name);
+eventRoot.dispatch(SendEmail.name);
+eventRoot.unsubscribe(SendMessage.name);
+eventRoot.unsubscribe(SendEmail.name);
+
 // src/observer/observer-root.ts
 var Observer = class {
   events = [];
@@ -38,18 +151,18 @@ var Observer = class {
 };
 
 // src/observer/email.ts
-var Email = class extends Entity {
+var Email2 = class extends Entity {
   name = "email-event";
   constructor(props) {
     super(props);
   }
-  notify(eventRoot) {
+  notify(eventRoot2) {
     console.log({
       email: this.props.email,
       content: this.props.content,
       title: this.props.title,
       to: this.props.to,
-      eventRootName: eventRoot.eventRootName
+      eventRootName: eventRoot2.eventRootName
     });
   }
 };
@@ -65,11 +178,11 @@ var Message = class extends Entity {
   constructor(props) {
     super(props);
   }
-  notify(eventRoot) {
+  notify(eventRoot2) {
     console.log({
       number: this.props.number,
       contactName: this.props.contactName,
-      eventRootName: eventRoot.eventRootName
+      eventRootName: eventRoot2.eventRootName
     });
   }
 };
@@ -82,7 +195,7 @@ var MessagesObservers = class extends Observer {
 // src/observer/index.ts
 var notifications = new EmailObservers("send-emails");
 var messages = new MessagesObservers("send-messages");
-var email = new Email({
+var email = new Email2({
   content: "itaque voluptas aut",
   email: "johndoe@johndoe.com",
   title: "sit aspernatur aut odit aut fugit",
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import "events";
-import "observer";
+import "events/index";
+import "observer/index";
